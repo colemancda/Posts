f2115a9505f89fbffd499f56467809b6e48bd930
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-09-24 21:30:43 -0400

    first commit

---
+++ Posts/Posts/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Posts
+//
+//  Created by Alsey Coleman Miller on 9/24/14.
+//  Copyright (c) 2014 ColemanCDA. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
