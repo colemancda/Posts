f31fa1e8e7da75ddd37d672809a449e436e5439e
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-09-24 21:45:31 -0400

    added submodule

---
+++ Server/PostsServer/PostsServer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PostsServer
+//
+//  Created by Alsey Coleman Miller on 9/24/14.
+//  Copyright (c) 2014 ColemanCDA. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ Shared/CorePosts/CorePosts/CorePosts.h
@@ -0,0 +1,19 @@
+//
+//  CorePosts.h
+//  CorePosts
+//
+//  Created by Alsey Coleman Miller on 9/24/14.
+//  Copyright (c) 2014 ColemanCDA. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CorePosts.
+FOUNDATION_EXPORT double CorePostsVersionNumber;
+
+//! Project version string for CorePosts.
+FOUNDATION_EXPORT const unsigned char CorePostsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CorePosts/PublicHeader.h>
+
+
