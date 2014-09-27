1f80d56a95128a257891582151dc4d8c52409616
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-09-27 13:32:44 -0400

    changed bundle id of CorePosts

--- Server/TempPostsServer/TempPostsServer/AppDelegate.swift
+++ Server/TempPostsServer/TempPostsServer/AppDelegate.swift
@@ -14,9 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // start server
+        ServerManager.sharedManager.server.start(onPort: 8080)
+        
         return true
     }
 
