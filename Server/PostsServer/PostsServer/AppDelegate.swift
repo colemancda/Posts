e5261079b879da70b73d1e0a07da367bd5d9c09d
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-10-04 10:12:40 -0400

    updated model

--- Server/PostsServer/PostsServer/AppDelegate.swift
+++ Server/PostsServer/PostsServer/AppDelegate.swift
@@ -32,8 +32,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationWillTerminate(aNotification: NSNotification?) {
         // Insert code here to tear down your application
+        
+        ServerManager.sharedManager.server.stop()
     }
-
-
+    
 }
 
