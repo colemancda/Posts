db13f9601558bdd36a3f3ff9128cc71a70d7cfc6
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-10-12 12:25:17 -0400

    Made Server a command line tool

---
+++ Server/PostServerDaemon/PostServerDaemon/main.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  PostServerDaemon
+//
+//  Created by Alsey Coleman Miller on 10/12/14.
+//  Copyright (c) 2014 ColemanCDA. All rights reserved.
+//
+
+import Foundation
+
+// start server
+let port = 8080 as UInt
+
+func initialize() {
+    
+    let error = ServerManager.sharedManager.server.start(onPort: port)
+    
+    if error != nil {
+        
+        println("Could not start server. (\(error))")
+        
+        return
+    }
+    
+    println("Started server on port \(port)")
+}
+
+initialize()
+
+
